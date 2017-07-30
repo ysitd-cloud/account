c500e015f41962551b6fc158cd258e4d74b60509
Tony Yip <tony@opensource.hk>
2017-07-30 15:33:27 +0800

    Add judge token auth middleware

--- http/middlewares/auth.go
+++ http/middlewares/auth.go
@@ -5,23 +5,63 @@ import (
 	"strings"
 
 	"github.com/RangelReale/osin"
+	"github.com/ysitd-cloud/judge-go-client"
 	"gopkg.in/gin-gonic/gin.v1"
 )
 
 func BearerToken(c *gin.Context) {
-	token := c.MustGet("authorization.value").(string)
-	server := c.MustGet("osin.server").(*osin.Server)
 
 	if c.MustGet("authorization.type").(string) != "bearer" {
 		c.Next()
 		return
 	}
 
+	server := c.MustGet("osin.server").(*osin.Server)
+	token := c.MustGet("authorization.value").(string)
+
 	if access, err := server.Storage.LoadAccess(token); err != nil {
 		c.AbortWithStatus(http.StatusForbidden)
 	} else {
 		c.Set("oauth.access", access)
 		c.Next()
+	}
+}
+
+func JudgeToken(action, resource string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		if c.MustGet("authorization.type").(string) != "judge" {
+			c.Next()
+			return
+		}
+		token := c.MustGet("authorization.value").(string)
+
+		subjectHeader := c.GetHeader("X-Client")
+		pieces := strings.Split(subjectHeader, ":")
+		if len(pieces) != 2 {
+			c.AbortWithStatus(http.StatusBadRequest)
+			return
+		}
+		subjectType := judge.SubjectType(pieces[0])
+		subject := pieces[1]
+		client :=
+			c.MustGet("judge").(judge.Client)
+		result, reason, errors := client.Judge(subject, subjectType, action, resource, token)
+		if len(errors) > 0 {
+			c.AbortWithError(http.StatusBadGateway, errors[0])
+			return
+		}
+
+		if result {
+			c.Next()
+			return
+		}
+
+		if reason == "forbidden" {
+			c.AbortWithStatus(http.StatusForbidden)
+			return
+		}
+
+		c.AbortWithStatus(http.StatusServiceUnavailable)
 	}
 }
 
@@ -38,9 +78,9 @@ func ContainsAuthHeader(c *gin.Context) {
 		return
 	}
 
-	authType := pieces[0]
+	authType := strings.ToLower(pieces[0])
 
-	if authType != "bearer" && authType != "token" {
+	if authType != "bearer" && authType != "judge" {
 		c.AbortWithStatus(http.StatusBadRequest)
 		return
 	}
