4eaeda37556bc3bd4f947712c2c09546086fb086
Tony Yip <tony@opensource.hk>
2017-08-02 05:20:25 +0800

    Trial with jet template

---
+++ http/helper/view.go
@@ -0,0 +1,19 @@
+package helper
+
+import (
+	"fmt"
+	"github.com/CloudyKit/jet"
+	"gopkg.in/gin-gonic/gin.v1"
+	"os"
+)
+
+var host string = os.Getenv("STATIC_ADDRESS")
+
+func RenderAppView(c *gin.Context, code int, view, title string) {
+	vars := make(jet.VarMap)
+	vars.Set("title", title)
+	vars.Set("view", view)
+	vars.Set("script", fmt.Sprintf("%s/%s/app.js", host, view))
+	vars.Set("style", fmt.Sprintf("%s/%s/app.css", host, view))
+	c.HTML(code, "app.jet", nil)
+}
--- main.go
+++ main.go
@@ -3,14 +3,18 @@ package main
 import (
 	"os"
 
+	"github.com/CloudyKit/jet"
 	_ "github.com/joho/godotenv/autoload"
 	"github.com/ysitd-cloud/account/http"
+	render "github.com/ysitd-cloud/gin-jet"
 	"gopkg.in/gin-gonic/gin.v1"
 )
 
 func main() {
 	app := gin.Default()
-	app.LoadHTMLGlob("views/*.tmpl")
+	views := jet.NewHTMLSet("./views")
+	template := render.NewJetRender(views)
+	app.HTMLRender = template
 
 	http.Register(app)
 
