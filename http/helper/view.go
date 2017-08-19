098c41bb3e8f5ac0d25173aafe6736d189ba851d
Tony Yip <tony@opensource.hk>
2017-08-20 01:53:02 +0800

    Update view

--- http/helper/view.go
+++ http/helper/view.go
@@ -12,9 +12,11 @@ var host string = os.Getenv("STATIC_ADDRESS")
 
 func RenderAppView(c *gin.Context, code int, view, title string) {
 	vars := make(jet.VarMap)
+	staticPath := fmt.Sprintf("%s/assets", host)
 	vars.Set("title", title)
 	vars.Set("view", view)
-	vars.Set("script", fmt.Sprintf("%s/assets/%s/app.js", host, view))
-	vars.Set("style", fmt.Sprintf("%s/assets/%s/app.css", host, view))
+	vars.Set("script", fmt.Sprintf("%s/%s/app.js", staticPath, view))
+	vars.Set("style", fmt.Sprintf("%s/%s/app.css", staticPath, view))
+	vars.Set("staticPath", staticPath)
 	c.HTML(code, "app.jet", vars)
 }
