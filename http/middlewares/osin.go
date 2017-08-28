4ff7159c58af65ed68e2bf6cc4a1f07fdbcca78a
Tony Yip <tony@opensource.hk>
2017-08-29 01:42:15 +0800

    Add close for osin server

--- http/middlewares/osin.go
+++ http/middlewares/osin.go
@@ -8,6 +8,7 @@ import (
 func Osin() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		server := setup.SetupOsinServer()
+		defer server.Storage.Close()
 		c.Set("osin.server", server)
 		c.Next()
 	}
