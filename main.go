ab044b085087d609079e2155ca6d86407e18ac38
Tony Yip <tony@opensource.hk>
2017-09-14 14:59:26 +0800

    Fix for session package

--- main.go
+++ main.go
@@ -8,10 +8,16 @@ import (
 	_ "github.com/joho/godotenv/autoload"
 	"github.com/ysitd-cloud/account/http"
 	render "github.com/ysitd-cloud/gin-jet"
+	"github.com/gin-contrib/pprof"
 )
 
 func main() {
 	app := gin.Default()
+
+	if gin.Mode() != gin.ReleaseMode {
+		pprof.Register(app, nil)
+	}
+
 	views := jet.NewHTMLSet("./views")
 	template := render.NewJetRender(views)
 	app.HTMLRender = template
