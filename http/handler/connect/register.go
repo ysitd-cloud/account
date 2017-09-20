384d4a21ae0d77de274e7cb90bc2565c8bfe0740
Tony Yip <tony@opensource.hk>
2017-09-21 00:38:42 +0800

    Add connect social account page

--- http/handler/connect/register.go
+++ http/handler/connect/register.go
@@ -6,6 +6,7 @@ import (
 )
 
 func Register(app *gin.Engine) {
+	app.GET("/connect", middlewares.LoginOrRedirect, listConnect)
 	group := app.Group("/connect", middlewares.LoginOrRedirect)
 	group.GET("/:provider", middlewares.LoginOrRedirect, redirectToOAuth)
 	group.GET("/:provider/callback", middlewares.LoginOrRedirect, oauthCallback)
