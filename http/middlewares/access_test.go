538b6a89c114e9e8b1729bcfe05d9b2b47b72665
Tony Yip <tony@opensource.hk>
2017-09-21 17:27:38 +0800

    Add unit test for middleware

---
+++ http/middlewares/access_test.go
@@ -0,0 +1,60 @@
+package middlewares
+
+import (
+	"net/http/httptest"
+	"testing"
+
+	"net/http"
+
+	"github.com/RangelReale/osin"
+	"github.com/gin-gonic/gin"
+)
+
+func TestCheckGetUserAccessAccept(t *testing.T) {
+	gin.SetMode(gin.ReleaseMode)
+	w := httptest.NewRecorder()
+	c, _ := gin.CreateTestContext(w)
+	c.Params = append(c.Params, gin.Param{
+		Key:   "user",
+		Value: "foo",
+	})
+
+	access := &osin.AccessData{
+		UserData: "foo",
+	}
+
+	c.Set("oauth.access", access)
+
+	CheckGetUserAccess(c)
+
+	if c.IsAborted() {
+		t.Error("Context should not abort", c)
+	}
+}
+
+func TestCheckGetUserAccessForbidden(t *testing.T) {
+	gin.SetMode(gin.ReleaseMode)
+	w := httptest.NewRecorder()
+	c, _ := gin.CreateTestContext(w)
+	c.Params = append(c.Params, gin.Param{
+		Key:   "user",
+		Value: "foo",
+	})
+
+	access := &osin.AccessData{
+		UserData: "bar",
+	}
+
+	c.Set("oauth.access", access)
+
+	CheckGetUserAccess(c)
+
+	if !c.IsAborted() {
+		t.Error("Context should abort", c)
+	}
+
+	resp := w.Result()
+	if resp.StatusCode != http.StatusForbidden {
+		t.Error("Wrong status code", resp.StatusCode)
+	}
+}
