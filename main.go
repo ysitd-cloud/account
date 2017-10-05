0fa08048615de42743ab709326d0f09e25a2200e
Tony Yip <tony@opensource.hk>
2017-10-06 00:15:40 +0800

    Remove old jet

--- main.go
+++ main.go
@@ -3,12 +3,10 @@ package main
 import (
 	"os"
 
-	"github.com/CloudyKit/jet"
 	"github.com/gin-contrib/pprof"
 	"github.com/gin-gonic/gin"
 	_ "github.com/joho/godotenv/autoload"
 	"github.com/ysitd-cloud/account/http"
-	render "github.com/ysitd-cloud/gin-jet"
 )
 
 func main() {
@@ -17,10 +15,6 @@ func main() {
 	if gin.Mode() != gin.ReleaseMode {
 		pprof.Register(app, nil)
 	}
-
-	views := jet.NewHTMLSet("./views")
-	template := render.NewJetRender(views)
-	app.HTMLRender = template
 
 	http.Register(app)
 
