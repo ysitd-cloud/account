27cfdb817d5e0d4d0d7c7c804077ef89f8918edd
Tony Yip <tony@opensource.hk>
2017-10-12 12:11:04 +0800

    Remove duplicate middlewares

--- http/handler/connect/register.go
+++ http/handler/connect/register.go
@@ -8,6 +8,6 @@ import (
 func Register(app *gin.Engine) {
 	app.GET("/connect", middlewares.LoginOrRedirect, listConnect)
 	group := app.Group("/connect", middlewares.LoginOrRedirect)
-	group.GET("/:provider", middlewares.LoginOrRedirect, redirectToOAuth)
-	group.GET("/:provider/callback", middlewares.LoginOrRedirect, oauthCallback)
+	group.GET("/:provider", redirectToOAuth)
+	group.GET("/:provider/callback", oauthCallback)
 }
