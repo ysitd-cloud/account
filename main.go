d921507659ba902ec744aa97b598c04879b1e950
Tony Yip <tony@opensource.hk>
2017-12-31 21:36:06 +0800

    Add grpc proxy to main

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gin-contrib/pprof"
 	"github.com/gin-gonic/gin"
 	_ "github.com/joho/godotenv/autoload"
+	proxy2 "github.com/ysitd-cloud/account/pkg/grpc/proxy"
 	"github.com/ysitd-cloud/account/pkg/http"
 	"github.com/ysitd-cloud/account/pkg/kernel"
 	"github.com/ysitd-cloud/account/pkg/providers"
@@ -29,6 +30,10 @@ func main() {
 	http.Register(app)
 
 	go app.Run(ginNet.GetAddress())
+
+	proxy := kernel.Kernel.Make("grpc.proxy").(proxy2.GrpcProxy)
+	engine := proxy.CreateApp()
+	go engine.Run(":50050")
 
 	server := kernel.Kernel.Make("grpc.server").(*grpc.Server)
 	listener := kernel.Kernel.Make("grpc.listener").(net.Listener)
--- pkg/providers/grpc.go
+++ pkg/providers/grpc.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/tonyhhyip/go-di-container"
 	grpcService "github.com/ysitd-cloud/account/pkg/grpc"
+	"github.com/ysitd-cloud/account/pkg/grpc/proxy"
 	"github.com/ysitd-cloud/account/pkg/utils"
 	pb "github.com/ysitd-cloud/grpc-schema/account"
 	"google.golang.org/grpc"
@@ -62,5 +63,10 @@ func (*grpcServiceProvder) Register(app container.Container) {
 		pb.RegisterAccountServer(server, service)
 
 		return server
+	})
+
+	app.Singleton("grpc.proxy", func(app container.Container) interface{} {
+		service := app.Make("grpc.service").(pb.AccountServer)
+		return proxy.CreateProxy(service)
 	})
 }
