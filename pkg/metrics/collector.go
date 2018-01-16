f328d167e62d2c573ad7fa0b69f58290f3573281
Tony Yip <tony@opensource.hk>
2018-01-17 00:35:00 +0800

    Move out init part for collector

--- pkg/metrics/collector.go
+++ pkg/metrics/collector.go
@@ -5,3 +5,49 @@ import "github.com/prometheus/client_golang/prometheus"
 func (c *collector) GetGatherer() prometheus.Gatherer {
 	return c.registry
 }
+
+func (c *collector) init() {
+	c.initHttpCollector()
+	c.initRpcCollector()
+	c.registry.MustRegister(c.rpc.timer, c.rpc.total, c.http.timer, c.http.total)
+}
+
+func (c *collector) initRpcCollector() {
+	counter := prometheus.NewCounterVec(prometheus.CounterOpts{
+		Subsystem: "rpc",
+		Name:      "count",
+		Help:      "RPC total count",
+		ConstLabels: prometheus.Labels{
+			"component": "account",
+		},
+	}, []string{"result", "endpoint"})
+	timer := prometheus.NewHistogramVec(prometheus.HistogramOpts{
+		Subsystem: "rpc",
+		Name:      "duration",
+		Help:      "RPC call duration",
+		ConstLabels: prometheus.Labels{
+			"component": "account",
+		},
+	}, []string{"result", "endpoint"})
+	c.rpc = newRPCCollector(counter, timer)
+}
+
+func (c *collector) initHttpCollector() {
+	counter := prometheus.NewCounterVec(prometheus.CounterOpts{
+		Subsystem: "http",
+		Name:      "count",
+		Help:      "Http total count",
+		ConstLabels: prometheus.Labels{
+			"component": "account",
+		},
+	}, []string{"code", "endpoint"})
+	timer := prometheus.NewHistogramVec(prometheus.HistogramOpts{
+		Subsystem: "http",
+		Name:      "duration",
+		Help:      "Http call duration",
+		ConstLabels: prometheus.Labels{
+			"component": "account",
+		},
+	}, []string{"code", "endpoint"})
+	c.http = newRPCCollector(counter, timer)
+}
--- pkg/metrics/constructor.go
+++ pkg/metrics/constructor.go
@@ -16,54 +16,15 @@ func NewServiceProvider(app container.Container) container.ServiceProvider {
 }
 
 func NewCollector(registry registry) Collector {
-	var rpc, http *rpcCollector
-	{
-		counter := prometheus.NewCounterVec(prometheus.CounterOpts{
-			Subsystem: "rpc",
-			Name:      "count",
-			Help:      "RPC total count",
-			ConstLabels: prometheus.Labels{
-				"component": "account",
-			},
-		}, []string{"result", "endpoint"})
-		timer := prometheus.NewHistogramVec(prometheus.HistogramOpts{
-			Subsystem: "rpc",
-			Name:      "duration",
-			Help:      "RPC call duration",
-			ConstLabels: prometheus.Labels{
-				"component": "account",
-			},
-		}, []string{"result", "endpoint"})
-		rpc = newRPCCollector(counter, timer)
-	}
-
-	{
-		counter := prometheus.NewCounterVec(prometheus.CounterOpts{
-			Subsystem: "http",
-			Name:      "count",
-			Help:      "Http total count",
-			ConstLabels: prometheus.Labels{
-				"component": "account",
-			},
-		}, []string{"code", "endpoint"})
-		timer := prometheus.NewHistogramVec(prometheus.HistogramOpts{
-			Subsystem: "http",
-			Name:      "duration",
-			Help:      "Http call duration",
-			ConstLabels: prometheus.Labels{
-				"component": "account",
-			},
-		}, []string{"code", "endpoint"})
-		http = newRPCCollector(counter, timer)
-	}
-
-	return &collector{
-		rpc:           rpc,
-		http:          http,
+	c := &collector{
 		rpcEndpoints:  make(map[string]*rpcCollector),
 		httpEndpoints: make(map[string]*rpcCollector),
 		registry:      registry,
 	}
+
+	c.init()
+
+	return c
 }
 
 func newRPCCollector(total *prometheus.CounterVec, timer *prometheus.HistogramVec) *rpcCollector {
