3af0525a15521874f7dbc8582f27919a56f60315
Tony Yip <tony@opensource.hk>
2018-04-13 17:36:12 +0800

    Start over

--- pkg/kernel/const.go
+++
@@ -1,5 +0,0 @@
-package kernel
-
-import "github.com/tonyhhyip/go-di-container"
-
-var Kernel = container.NewKernel()
--- pkg/metrics/rpc_collector.go
+++
@@ -1,7 +0,0 @@
-package metrics
-
-import "github.com/prometheus/client_golang/prometheus"
-
-func (c *rpcCollector) register(registerer prometheus.Registerer) {
-	registerer.MustRegister(c.total, c.timer)
-}
