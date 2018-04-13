e2ddefbbfec6e9a3fcbae5564932b523d038666a
Tony Yip <tony@opensource.hk>
2018-04-14 01:55:40 +0800

    Create http handler provider

---
+++ pkg/server/public/provider.go
@@ -0,0 +1,33 @@
+package public
+
+import (
+	"net/http"
+	"os"
+
+	"code.ysitd.cloud/component/account/pkg/service"
+	"github.com/gorilla/handlers"
+	"github.com/tonyhhyip/vodka"
+)
+
+type Provider struct {
+	Service *service.Service `inject:""`
+}
+
+func (p *Provider) CreateHandler() http.Handler {
+	router := p.createRouter()
+	handler := router.Handler()
+	s := vodka.New("")
+	s.StandBy(handler)
+	return handlers.CombinedLoggingHandler(os.Stdout, s.Server.Handler)
+}
+
+func (p *Provider) createRouter() (r *vodka.Router) {
+	r = vodka.NewRouter()
+	r.Use(&securityMiddleware{
+		FrameDeny:          true,
+		BrowserXssFilter:   true,
+		ContentTypeNosniff: true,
+		STSSeconds:         5184000,
+	})
+	return
+}
