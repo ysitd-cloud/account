cd0085946643f81cb11b99da15aa25bf309fe680
Tony Yip <tony@opensource.hk>
2018-04-29 20:46:11 +0800

    Change gorequest to http.Client

--- pkg/http/helper/view.go
+++ pkg/http/helper/view.go
@@ -6,20 +6,26 @@ import (
 	"net/url"
 	"os"
 
+	"bytes"
 	"code.ysitd.cloud/auth/account/pkg/http/middlewares"
 	"code.ysitd.cloud/auth/account/pkg/model/user"
+	"encoding/json"
 	"github.com/gin-gonic/gin"
-	"github.com/parnurzeal/gorequest"
+	"io"
+	"time"
 )
 
 var sidecarHost string
+
+var client = &http.Client{
+	Timeout: 10 * time.Second,
+}
 
 func init() {
 	sidecarHost = os.Getenv("SIDECAR_URL")
 }
 
 func RenderAppView(c *gin.Context, code int, view string, data map[string]interface{}) {
-
 	if session := middlewares.GetSession(c); session.Exists("username") {
 		if data == nil {
 			data = make(map[string]interface{})
@@ -39,20 +45,34 @@ func RenderAppView(c *gin.Context, code int, view string, data map[string]interf
 		return
 	}
 
-	req := gorequest.New()
-
 	sideCarURL.Path = fmt.Sprintf("/%s", view)
 	sideCarURL.RawQuery = c.Request.URL.RawQuery
 
-	_, body, errs := req.
-		Post(sideCarURL.String()).
-		Send(data).
-		End()
-	if len(errs) != 0 {
-		c.AbortWithError(http.StatusBadGateway, errs[0])
+	body, err := json.Marshal(data)
+	if err != nil {
+		c.AbortWithError(http.StatusInternalServerError, err)
+		return
 	}
 
+	req, err := http.NewRequest("POST", sideCarURL.String(), bytes.NewReader(body))
+	if err != nil {
+		c.AbortWithError(http.StatusInternalServerError, err)
+		return
+	}
+
+	res, err := client.Do(req)
+	if err != nil {
+		c.AbortWithError(http.StatusBadGateway, err)
+	}
+
+	defer res.Body.Close()
+
 	c.Status(code)
-	c.Writer.WriteString(body)
+
+	if _, err = io.Copy(c.Writer, res.Body); err != nil {
+		c.AbortWithError(http.StatusInternalServerError, err)
+		return
+	}
+
 	c.Abort()
 }
