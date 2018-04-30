981ef9d0f951c1062d2f35861bd5c3d15337c669
Tony Yip <tony@opensource.hk>
2018-04-30 16:06:25 +0800

    Inject osin

--- pkg/providers/osin.go
+++ pkg/providers/osin.go
@@ -2,11 +2,31 @@ package providers
 
 import (
 	"code.ysitd.cloud/auth/account/pkg/storage"
-	"github.com/RangelReale/osin"
+	"code.ysitd.cloud/auth/account/third_party/forked/github.com/RangelReale/osin"
+	"github.com/facebookgo/inject"
 	"github.com/gomodule/redigo/redis"
 	"github.com/tonyhhyip/go-di-container"
 	"golang.ysitd.cloud/db"
+	"time"
 )
+
+func initOsinConfig() (config *osin.ServerConfig) {
+	config = osin.NewServerConfig()
+	config.AllowedAccessTypes = osin.AllowedAccessType{osin.AuthorizationCode, osin.RefreshToken}
+	config.AllowClientSecretInParams = true
+	config.ErrorStatusCode = 400
+	return
+}
+
+func InjectOsin(graph *inject.Graph) {
+	graph.Provide(
+		NewObject(initOsinConfig()),
+		NewNamedObject("osin storage", new(storage.Store)),
+		NewNamedObject("osin authorize token gen", new(osin.AuthorizeTokenGenDefault)),
+		NewNamedObject("osin access token gen", new(osin.AccessTokenGenDefault)),
+		NewNamedObject("osin now func", func() time.Time { return time.Now().UTC() }),
+	)
+}
 
 type osinServiceProvider struct {
 	*container.AbstractServiceProvider
@@ -22,14 +42,14 @@ func (*osinServiceProvider) Provides() []string {
 
 func (*osinServiceProvider) Register(app container.Container) {
 	app.Bind("osin.storage", func(app container.Container) interface{} {
-		db := app.Make("db.pool").(db.Opener)
+		db := app.Make("db.pool").(*db.GeneralOpener)
 		redisDB := app.Make("redis.pool").(*redis.Pool)
 		return storage.NewStore(db, redisDB)
 	})
 
 	app.Singleton("osin.config", func(app container.Container) interface{} {
 		config := osin.NewServerConfig()
-		config.AllowedAccessTypes = osin.AllowedAccessType{osin.AUTHORIZATION_CODE, osin.REFRESH_TOKEN}
+		config.AllowedAccessTypes = osin.AllowedAccessType{osin.AuthorizationCode, osin.RefreshToken}
 		config.AllowClientSecretInParams = true
 		config.ErrorStatusCode = 400
 		return config
